8fd34ae7ae2bb22af4de7fcfcba840e3fd15c6b5
Peter Yu <mr.peteryu@gmail.com>
2019-09-06 17:58:38 -0400

    add support for heic

--- fotoorg/Extensions/FileExtensions.cs
+++ fotoorg/Extensions/FileExtensions.cs
@@ -6,7 +6,7 @@ namespace fotoorg
     public static class FileExtensions
     {
         public static bool IsPhoto(this string fileName)
-            => new[] { ".jpg", ".png", ".gif" }.Any(x => x == Path.GetExtension(fileName).ToLower());
+            => new[] { ".jpg", ".png", ".gif", ".heic" }.Any(x => x == Path.GetExtension(fileName).ToLower());
 
         public static bool IsVideo(this string fileName)
             => new[] { ".mov", ".avi", ".mp4", ".mpg" }.Any(x => x == Path.GetExtension(fileName).ToLower());
